de76b1d57deba6b3abb136342c090a53d0ac3e2d
Hiro? <brenofprocopio@gmail.com>
2022-09-23 09:47:57 -0300

    v0.830
    +conserto

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
+import { HeaderComponent } from './header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: ContentComponent
+  },
+  {
+    path: '', component: HeaderComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
