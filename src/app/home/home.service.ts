d53b8c66d26f87761691d7924adf01132f289074
Hiro? <brenofprocopio@gmail.com>
2022-10-25 19:27:32 -0300

    25/10 - Alterações
    + updates

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Produtos } from '../core/model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +11,11 @@ export class HomeService {
 
   constructor(private http: HttpClient) {}
 
-  list() {
-    return this.http.get<Produtos[]>(this.API);
+  list(): Observable<Produtos[]> {
+    return this.http.get<Produtos[]>(`${this.API}/todos`).pipe(retorno => retorno);
+  }
+
+  listarProdutoId(id: number): Observable<Produtos> {
+    return this.http.get<Produtos>(`${this.API}/${id}`).pipe(retorno => retorno);
   }
 }
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -2,20 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { Produtos } from 'src/app/core/model';
 import { Observable } from 'rxjs';
 import { HomeService } from '../home.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
+
 export class HomeComponent implements OnInit {
   value = 'Pesquisar';
 
   produtos: Observable<Produtos[]>;
   api = ['nomeproduto', 'produtopreco'];
-  constructor(private homeService: HomeService) {
+  constructor(
+    private homeService: HomeService,
+    private activatedRoute: ActivatedRoute
+    )
+    {
     this.produtos = this.homeService.list();
   }
 
   ngOnInit(): void {}
+
+  // exibirProduto() {
+  //   const id = Number(this.activatedRoute.snapshot.paramMap.get('idProdutos'));
+  //   this.homeService.listarProdutoId(1).subscribe(retorno => {
+  //     this.produtos = retorno;
+  //   })
+  // }
 }
