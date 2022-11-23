e7a1747509182ec6f1c5aa8a557a875fc1b1b227
beatrizbrandao <beatrizbrandao764@gmail.com>
2022-11-23 12:19:43 -0300

    23/11

--- src/app/mans/mans.module.ts
+++ src/app/mans/mans.module.ts
@@ -3,6 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { MansRoutingModule } from './mans-routing.module';
 import { MansComponent } from './mans/mans.component';
+import { MatBadgeModule } from '@angular/material/badge';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule } from '@angular/material/input';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 @NgModule({
@@ -11,7 +19,16 @@ import { MansComponent } from './mans/mans.component';
   ],
   imports: [
     CommonModule,
-    MansRoutingModule
+    MansRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    NgbModule,
+    MatInputModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatBadgeModule,
   ]
 })
 export class MansModule { }
--- src/app/mans/mans/mans.component.ts
+++ src/app/mans/mans/mans.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { CarrinhoapiService } from 'src/app/services/carrinhoapi.service';
 
 @Component({
   selector: 'app-mans',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MansComponent implements OnInit {
 
-  constructor() { }
+  value = 'Pesquisar';
+
+  public productList: any;
+  public totalItem: number = 0;
+  constructor(
+    private api: ApiService,
+    private cartService: CarrinhoapiService
+  ) {}
 
   ngOnInit(): void {
+    this.api.getProduct().subscribe((retorno) => {
+      this.productList = retorno;
+
+      this.productList.forEach((a: any) => {
+        Object.assign(a, { quantity: 1, total: a.produtopreco });
+      });
+    });
+
+    this.cartService.getProducts().subscribe((res) => {
+      this.totalItem = res.length;
+    });
+  }
+
+  addtocart(item: any) {
+    this.cartService.addtoCart(item);
   }
 
 }
