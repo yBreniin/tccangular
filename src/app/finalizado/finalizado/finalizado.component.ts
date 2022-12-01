458134a133a4673a26ec9dd288b4116b4a5c7333
beatrizbrandao <beatrizbrandao764@gmail.com>
2022-12-01 12:19:59 -0300

    01/12

--- src/app/finalizado/finalizado/finalizado.component.ts
+++ src/app/finalizado/finalizado/finalizado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarrinhoapiService } from 'src/app/services/carrinhoapi.service';
 
 @Component({
   selector: 'app-finalizado',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./finalizado.component.scss']
 })
 export class FinalizadoComponent implements OnInit {
+  value = 'Pesquisar';
 
-  constructor() { }
-
+  public products: any = [];
+  public grandTotal = 0;
+  constructor(
+    private cartService: CarrinhoapiService
+  ) {}
   ngOnInit(): void {
+    this.cartService.getProducts().subscribe((res) => {
+      this.products = res;
+      this.grandTotal = this.cartService.getTotalPrice();
+    });
+  }
+
+  removeItem(item: any) {
+    this.cartService.removeCartItem(item);
+  }
+
+  emptycart() {
+    this.cartService.removeAllCart();
   }
 
 }
